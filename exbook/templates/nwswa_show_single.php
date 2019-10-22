192c6a2f5c1cddf1c6d0cf52e8e7e07852ee7da6
netzwerkstatt.cc <31471027+llukee@users.noreply.github.com>
2019-10-22 14:39:53 +0200

    Revert "New template file for the registration form"
    This reverts commit 914764000875e921e5bf07d21c7f2307274aba0e.

--- exbook/templates/nwswa_show_single.php
+++ exbook/templates/nwswa_show_single.php
@@ -1,90 +1,25 @@
-<?php
-/**
- * The template for displaying all single shows
- */
- 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-get_header();
-?>
+<header class="entry-header">	
+	<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+</header>
+	
+<div class="entry">
+ 		<?php the_content(); ?>
+</div>
+	
+<form id="form">
+<?php wp_nonce_field( 'contact_form_submit', 'cform_generate_nonce' );?>
+            <label>Name</label> <input type="text" name="name" class="text" id="name"><br>
+            <label>Email</label> <input type="email" name="email" class="text" id="email"><br>
 
-<section id="primary" class="content-area">
-		<main id="main" class="site-main">
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-				<header class="entry-header">	
-					<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-				</header>
-					
-				<div class="entry-content">
-					<?php the_content(); ?>
-					
-						
-					<form id="reservieren">
-					<h2>Reservieren</h2>
-					<?php wp_nonce_field( 'contact_form_submit', 'cform_generate_nonce' );?>
-
-					<p><label for="reservation_event">Vorstellung:</label>
-					<select id="reservation_event" name="reservation_event">
-						
-						<?php 
-						// Query the shows here
-						$query = new WP_Query( 'post_type=nwswa_event' );
-						while ( $query->have_posts() ) {
-									$option_text = '';
-							$query->the_post();
-									$event_id = get_the_ID();
-
-									// show title + event datetime
-									$show_id = get_post_meta( $event_id, 'nwswa_event_show', true );
-									$show = get_post($show_id);
-									$datetime_ts = get_post_meta( $event_id, 'nwswa_event_datetime', true );
-
-									$option_text .= $show->post_title;
-									$option_text .= ' - ';
-									$option_text .= date("d.m.Y H:i", $datetime_ts);
-
-							$selected = "";
-
-							if($event_id == $event){
-								$selected = ' selected="selected"';
-							}
-							echo '<option' . $selected . ' value=' . $event_id . '>' . $option_text . '</option>';
-						}
-					  ?>
-					  </select></p>
-
-								<p><label>Vorname</label> <input type="text" name="vorname" class="text" id="vorname"></p>
-								<p><label>Nachname</label> <input type="text" name="nachname" class="text" id="nachname"></p>
-								<p><label>Telefon</label> <input type="text" name="telefon" class="text" id="telefon"></p>
-								<p><label>Email</label> <input type="email" name="email" class="text" id="email"></p>
-								
-								<p><label for="reservation_quantity">Anzahl Plätze:</label>
-									<select name="reservation_quantity">
-									 <?php for($q=1;$q<=100;$q++) {
-										$selected = '';
-										if($q==$quantity) {
-											$selected = ' selected="selected" ';
-										}
-										echo '<option value="'.$q.'" '.$selected.'>'.$q.'</option>';
-									} ?>
-									</select></p>
-								
-								<p><label>News abonnieren?</label> <input type="checkbox"name="reservation_newsletter" checked="checked"></input></p>
-								
-								<input name="action" type="hidden" value="simple_contact_form_process" />
-								<p><input type="submit" name="submit_form" class="button" value="Reservierung absenden" id="sendmessage"></p>
-								
-								<div class="formmessage"><p>Das Formular wurde erfolgreich gesandt.</p></div>
-							</form>
-						
-					<?php endwhile; else : ?>
-						<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-					<?php endif; ?>
-				</div>	
-			</article><!-- #post-<?php the_ID(); ?> -->
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php
-get_footer();
+            <label>Subject</label> <input type="text" name="subject" class="text" id="subject"><br>
+            <label>Message</label><textarea id="message" class="textarea" name="message"></textarea>
+            <input name="action" type="hidden" value="simple_contact_form_process" />
+            <input type="submit" name="submit_form" class="button" value="send Message" id="sendmessage">
+            <div class="formmessage"><p></p></div>
+        </form>
+		
+<?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
