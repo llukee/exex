dda4f612bb9b6d95c563a704bb5c9585ada442a2
netzwerkstatt.cc <31471027+llukee@users.noreply.github.com>
2019-10-29 11:34:22 +0100

    Updated Form
    - Only show events in the future
    - Updated input names

--- exbook/templates/nwswa_show_single.php
+++ exbook/templates/nwswa_show_single.php
@@ -37,14 +37,34 @@ get_header();
 
 					<form id="reservation">
 					<h2>Reservieren</h2>
-					<?php wp_nonce_field( 'reservaton_form_submit', 'cform_generate_nonce' );?>
+					<?php wp_nonce_field( 'contact_form_submit', 'cform_generate_nonce' );?>
 
 					<p><label for="reservation_event">Vorstellung:</label>
 					<select id="reservation_event" name="reservation_event">
-
-						<?php
+						
+						<?php 
 						// Query the shows here
-						$query = new WP_Query( 'post_type=nwswa_event' );
+						$post_id = get_the_ID();
+						$args = array(
+								'post_type'         => 'nwswa_event',
+								'post_status'       => array( 'publish' ),
+								'posts_per_page'    => -1, // -1 = all posts
+								'meta_query' => array(
+									'relation' 				=> 'AND', // Optional, defaults to "OR"
+									'date_ordering' => array(
+										'key'  		=> 'nwswa_event_datetime',
+										'value' => date( "U" ),
+										'compare' => '>'
+									),
+									array(
+										'key' => 'nwswa_event_show',
+										'value' => $post_id,
+									)
+								),
+							'orderby' => 'date_ordering',
+							'order' => 'ASC',
+							);
+						$query = new WP_Query( $args );
 						while ( $query->have_posts() ) {
 									$option_text = '';
 							$query->the_post();
@@ -69,14 +89,15 @@ get_header();
 					  ?>
 					  </select></p>
 
-								<p><label>Vorname</label> <input type="text" name="vorname" class="text" id="vorname"></p>
-								<p><label>Nachname</label> <input type="text" name="nachname" class="text" id="nachname"></p>
-								<p><label>Telefon</label> <input type="text" name="telefon" class="text" id="telefon"></p>
-								<p><label>Email</label> <input type="email" name="email" class="text" id="email"></p>
-
-								<p><label for="reservation_quantity">Anzahl Plätze:</label>
+								<p><label>Vorname</label> <input type="text" name="reservation_firstname" class="text" id="vorname"></p>
+								<p><label>Nachname</label> <input type="text" name="reservation_lastname" class="text" id="nachname"></p>
+								<p><label>Telefon</label> <input type="text" name="reservation_phone" class="text" id="telefon"></p>
+								<p><label>E-Mail</label> <input type="email" name="reservation_email" class="text" id="email"></p>
+								
+								<p><label for="reservation_quantity">Anzahl Pl&auml;tze :</label>
+								
 									<select name="reservation_quantity">
-									 <?php for($q=1;$q<=100;$q++) {
+									 <?php for($q=1;$q<=10;$q++) {
 										$selected = '';
 										if($q==$quantity) {
 											$selected = ' selected="selected" ';
@@ -84,13 +105,13 @@ get_header();
 										echo '<option value="'.$q.'" '.$selected.'>'.$q.'</option>';
 									} ?>
 									</select></p>
-
+								
 								<p><label>News abonnieren?</label> <input type="checkbox"name="reservation_newsletter" checked="checked"></input></p>
-
+								
 								<input name="action" type="hidden" value="simple_contact_form_process" />
 								<p><input type="submit" name="submit_form" class="button" value="Reservierung absenden" id="sendmessage"></p>
-
-								<div class="formmessage hide"><p>Das Formular wurde erfolgreich gesandt.</p></div>
+								
+								<div class="formmessage"><p>Das Formular wurde erfolgreich gesandt.</p></div>
 							</form>
 
 					<?php endwhile; else : ?>
