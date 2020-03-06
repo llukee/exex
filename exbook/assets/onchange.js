aab2013936d273d2bf15d55ee3f7c1d2740bb76c
netzwerkstatt.cc <31471027+llukee@users.noreply.github.com>
2020-03-06 23:23:51 +0100

    Update onchange.js

--- exbook/assets/onchange.js
+++ exbook/assets/onchange.js
@@ -2,7 +2,7 @@
 							$(document).ready(function() {
 							$('.onchange').on('change', function() {
 							var $form = $(this).closest('form');
-							window.history.pushState({}, document.title, "/");
+							window.history.replaceState(null, null, window.location.pathname);
 							$form.find('input[type=submit]').click();
 							 });
 						});
