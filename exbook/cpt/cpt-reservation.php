eb2f566f10acaf5d655bd3e665b3b13eff5886bf
netzwerkstatt.cc <31471027+llukee@users.noreply.github.com>
2020-06-01 22:53:54 +0200

    Update cpt-reservation.php

--- exbook/cpt/cpt-reservation.php
+++ exbook/cpt/cpt-reservation.php
@@ -419,7 +419,7 @@ function add_extra_tablenav($post_type){
         ORDER BY "%3$s"',
         $wpdb->postmeta,
         $wpdb->posts,
-        'reservation_event', // Your meta key - change as required
+        'nwswa_event_show', // Your meta key - change as required
         'publish',          // Post status - change as required
         $post_type
     );
